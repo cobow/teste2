020855725e3c33316c35a0ce6359294180d636e2
LeLynXx <109920003+cobow@users.noreply.github.com>
2025-06-20 02:18:10 +0200

    Create logs.lua

---
+++ logs.lua
@@ -0,0 +1,10 @@
+KaitoLogs = {
+    IconLogs = "", -- Logo pour vos logs
+    NameLogs = "ðŸª Shiganshina",
+    DescriptionLogs = "KaitoCagoule",
+    CouleurLogs = "10038562", -- Couleur de vos logs
+    LogsCagouleMise = "",
+    LogsCagouleRetirer = "",
+}
+
+ 
