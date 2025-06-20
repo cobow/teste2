51aea35a8fac7dc567b0aaee916f63fbf88f5888
LeLynXx <109920003+cobow@users.noreply.github.com>
2025-06-20 02:16:00 +0200

    config.lua

---
+++ config.lua
@@ -0,0 +1,66 @@
+Kaito = {
+    TriggerLoad = 'esx:playerLoaded',
+    TriggerLogout = 'esx:onPlayerLogout',
+    TriggerNotif = 'esx:showNotification',
+    DistanceMaxPourCagoule = 2.0,
+    NomItemCagoule = 'sacp',
+    useOxInventory = true,
+    IconMettreCagoule = 'fa-solid fa-handcuffs',
+    IconRetirerCagoule = 'fa-solid fa-handcuffs',
+    Cagoule = {
+    male = {
+    ['mask_1'] = 136, ['mask_2'] = 2,
+    },
+    },
+    SansCagoule = {
+    male = {
+    ['mask_1'] = -1, ['mask_2'] = 0,
+    },
+    },
+    Notifications = {
+        Proximiter = {
+            title = 'Erreur',
+            description = 'Vous devez Ãªtre plus proche pour mettre la cagoule.',
+            type = 'error',
+            position = 'top-center'
+        },
+        CagouleMise = {
+            title = 'SuccÃ¨s',
+            description = 'La cagoule a Ã©tÃ© mise avec succÃ¨s.',
+            type = 'success',
+            position = 'top-center'
+        },
+        CagouleAlready = {
+            title = 'Information',
+            description = 'La cagoule est dÃ©jÃ  mise.',
+            type = 'info',
+            position = 'top-center'
+        },
+        CagouleAlreadyRetirer = {
+            title = 'Information',
+            description = 'La cagoule a Ã©tÃ© retirÃ©e.',
+            type = 'info',
+            position = 'top-center'
+        },
+        DejaUneCagoule = {
+            title = 'Erreur',
+            description = 'Vous portez dÃ©jÃ  une cagoule.',
+            type = 'error',
+            position = 'top-center'
+        },
+        MessageYouRetireCagoule = {
+            title = 'SuccÃ¨s',
+            description = 'Vous avez retirÃ© la cagoule.',
+            type = 'success',
+            position = 'top-center'
+        },
+        MessagePasDeCagouleSurLeGars = {
+            title = 'Erreur',
+            description = 'Il n\'y a pas de cagoule sur cette personne.',
+            type = 'error',
+            position = 'top-center'
+        }
+    }
+}
+
+ 
